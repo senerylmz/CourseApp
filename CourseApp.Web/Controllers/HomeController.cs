71a74b1ddb2fc22ae3a71df3981dcc3c24bfd233
senerylmz <seneryilmazzz@gmail.com>
2022-01-02 00:05:36 +0300

    First Project

---
+++ CourseApp.Web/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using CourseApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseApp.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var course = new Course();
+            course.Id = 1;
+            course.Name = "Asp.Net MVC";
+            return View(course);
+        }
+    }
+}
--- CourseApp.Web/Startup.cs
+++ CourseApp.Web/Startup.cs
@@ -16,6 +16,7 @@ namespace CourseApp.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +27,30 @@ namespace CourseApp.Web
                 app.UseDeveloperExceptionPage();
             }
 
+            //app.UseMvc(routes => 
+            //{ 
+            //    routes.MapRoute(
+            //        name: "default", 
+            //        template: "{controller=Home}/{action=Index}/{id?}"); 
+            //});
+            //app.UseStaticFiles();
             app.UseRouting();
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+            //app.MapControllerRoute(
+            //    name: "default",
+            //    pattern: "{controller=Home}/{action=Index}/{id?}");
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
         }
     }
 }
